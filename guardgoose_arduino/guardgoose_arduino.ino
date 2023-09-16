b6524a70c4e14680aad9479ad5010d772b20f437
Kailin <68765813+kailinchu@users.noreply.github.com>
2023-09-16 14:55:04 -0400

    Distance sensor implemented on arduino

--- guardgoose_arduino/guardgoose_arduino.ino
+++ guardgoose_arduino/guardgoose_arduino.ino
@@ -1,6 +1,8 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
+#include <VL53L1X.h>
+
 
 // PINS
 #define RED_EYE_1 23
@@ -17,6 +19,8 @@
 // address of BNO sensor: 0x28
 
 Adafruit_BNO055 bno;
+VL53L1X tof;
+
 void setup() {
   
   pinMode(RED_EYE_1, OUTPUT); 
@@ -26,35 +30,82 @@ void setup() {
   Serial.begin(9600);
   Serial.println("hello");
   Wire.begin();
-  if (!bno.begin()){
+
+  /* set up bno sensor */
+  if (!bno.begin()) {
     Serial.println("BNO IS LOOSE");
     while(1){};
   }
+  //calibrate bno
+  //  uint8_t system, gyro, accel, mag = 0;
+  //  bno.getCalibration(&system, &gyro, &accel, &mag);
 
-//calibrate bno
-//  uint8_t system, gyro, accel, mag = 0;
-//  bno.getCalibration(&system, &gyro, &accel, &mag);
+  /* set up time of flight sensor */
+  tof.setTimeout(500);
+  if (!tof.init()) {
+    Serial.println("TOF IS LOOSE");
+    while(1);
+  }
+  // short distance mode
+  tof.setDistanceMode(VL53L1X::Short);
+  tof.setMeasurementTimingBudget(20000);
+  tof.startContinuous(50);
 }
+
 float prev_xaccel, prev_yaccel, prev_zaccel =0;
 float acceleration_threshold = 0.9; //CHANGE THIS
+int prev_dist1, prev_dist2 = 500;
+
+int dist_threshold = 200;
+int avg_dist = 500;
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  /* Laptop has been moved */
+  // if the laptop screen is physically tilted (movement is detected), then red eyes and honking
   imu::Vector<3> accel_value = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
   
-  Serial.print("accel values:");
+  Serial.print("accel values: ");
   Serial.print(accel_value.x());
   Serial.print(", ");
   Serial.print(accel_value.y());
   Serial.print(", ");
   Serial.println(accel_value.z());
   if (accel_value.x()>=prev_xaccel){
-  digitalWrite(HONK_BUZZER,HIGH);
-  delay(1000);
-  digitalWrite(HONK_BUZZER, LOW);
+    digitalWrite(HONK_BUZZER,HIGH);
+    Serial.print("light on");
+    delay(1000);
+    digitalWrite(HONK_BUZZER, LOW);
   }
   prev_xaccel = accel_value.x();
   prev_yaccel = accel_value.y();
   prev_zaccel = accel_value.z();
+
+  /* Someone is approaching the laptop */
+  int distance = tof.read();
+  if (tof.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+  Serial.print("distance (mm): ");
+  Serial.println(distance);
+
+  avg_dist = (prev_dist1 + prev_dist2 + distance) / 3;
+  prev_dist2 = prev_dist1;
+  prev_dist1 = distance;
+
+  if (avg_dist < dist_threshold) {
+    redEyesOn(true);
+  } else {
+    redEyesOn(false);
+  }
   delay(1000);
-// if the laptop screen is physically tilted (movement is detected), then red eyes and honking
+
+}
+
+void redEyesOn(bool on) {
+  if (on) {
+    digitalWrite(RED_EYE_1,HIGH);
+    digitalWrite(RED_EYE_2,HIGH);
+  }
+  else {
+    digitalWrite(RED_EYE_1,LOW);
+    digitalWrite(RED_EYE_2,LOW);
+  }
 }
